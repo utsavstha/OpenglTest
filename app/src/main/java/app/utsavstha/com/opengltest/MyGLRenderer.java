6f48b90f3bfa30526f74279613edb66fd78718c6
Utsav Shrestha <utsavstha@outlook.com>
2016-12-24 11:02:13 +0545

    Added 2D rotation and created a cube.
    Todo:
    1: Figure out the indices.
    2: Add 3D rotation

--- app/src/main/java/app/utsavstha/com/opengltest/MyGLRenderer.java
+++ app/src/main/java/app/utsavstha/com/opengltest/MyGLRenderer.java
@@ -3,6 +3,7 @@ package app.utsavstha.com.opengltest;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
+import android.os.SystemClock;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -13,12 +14,16 @@ import javax.xml.transform.Transformer;
  */
 public class MyGLRenderer implements GLSurfaceView.Renderer {
 
+    public volatile float mAngle;
     private Triangle mTraingle;
     private Square mSquare;
+    private Cube mCube;
     // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
     private final float[] mMVPMatrix = new float[16];
     private final float[] mProjectionMatrix = new float[16];
     private final float[] mViewMatrix = new float[16];
+    //matrix for rotation
+    private float[] mRotationMatrix = new float[16];
 
     public void onSurfaceCreated(GL10 unused, EGLConfig config) {
         // Set the background frame color
@@ -31,11 +36,14 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         //initialising a square
         mSquare = new Square();
 
+        mCube = new Cube();
+
 
 
     }
 
     public void onDrawFrame(GL10 unused) {
+        float[] scratch = new float[16];
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
         // Set the camera position (View matrix)
@@ -44,7 +52,17 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         // Calculate the projection and view transformation
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
 
-        mTraingle.draw(mMVPMatrix);
+        // Create a rotation transformation for the triangle
+        //long time = SystemClock.uptimeMillis() % 4000L;
+        //float angle = 0.090f * ((int) time);
+        Matrix.setRotateM(mRotationMatrix, 0, mAngle, 0, 0, -1.0f);
+
+        // Combine the rotation matrix with the projection and camera view
+        // Note that the mMVPMatrix factor *must be first* in order
+        // for the matrix multiplication product to be correct.
+        Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
+
+        mCube.draw(scratch);
     }
 
     public void onSurfaceChanged(GL10 unused, int width, int height) {
@@ -67,5 +85,13 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         GLES20.glCompileShader(shader);
 
         return shader;
+    }
+
+    public float getAngle() {
+        return mAngle;
+    }
+
+    public void setAngle(float angle) {
+        mAngle = angle;
     }
 }
--- app/src/main/java/app/utsavstha/com/opengltest/MyGLSurfaceView.java
+++ app/src/main/java/app/utsavstha/com/opengltest/MyGLSurfaceView.java
@@ -2,11 +2,18 @@ package app.utsavstha.com.opengltest;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.support.v4.view.MotionEventCompat;
+import android.view.MotionEvent;
 
 /**
  * Created by utsav on 12/24/2016.
  */
-public class MyGLSurfaceView extends GLSurfaceView {
+public class MyGLSurfaceView extends GLSurfaceView  {
+
+    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+    private float mPreviousX;
+    private float mPreviousY;
+    private float mDensity;
 
     private final MyGLRenderer mRenderer;
 
@@ -18,7 +25,50 @@ public class MyGLSurfaceView extends GLSurfaceView {
 
         mRenderer = new MyGLRenderer();
 
+        // Render the view only when there is a change in the drawing data.
+        // To allow the triangle to rotate automatically, this line is commented out:
+
         // Set the Renderer for drawing on the GLSurfaceView
         setRenderer(mRenderer);
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        // MotionEvent reports input details from the touch screen
+        // and other input controls. In this case, you are only
+        // interested in events where the touch position changed.
+
+        float x = e.getX();
+        float y = e.getY();
+
+
+        switch (e.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+
+                float dx = x - mPreviousX;
+                float dy = y - mPreviousY;
+
+                // reverse direction of rotation above the mid-line
+                if (y > getHeight() / 2) {
+                    dx = dx * -1 ;
+                }
+
+                // reverse direction of rotation to left of the mid-line
+                if (x < getWidth() / 2) {
+                    dy = dy * -1 ;
+                }
+
+                mRenderer.setAngle(
+                        mRenderer.getAngle() +
+                                ((dx + dy) * TOUCH_SCALE_FACTOR));
+                requestRender();
+        }
+
+        mPreviousX = x;
+        mPreviousY = y;
+        return true;
     }
 }
